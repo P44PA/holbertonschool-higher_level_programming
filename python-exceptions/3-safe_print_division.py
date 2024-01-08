b60b22d15416585e7107f9c9ef23887333e6a9a2
root <dejvidpapa@gmail.com>
2024-01-08 06:27:59 -0800

    3-main.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print x elements of a list.
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2024 Dejvid Papa, Tirana ,Albania
+# email dejvidpapa@gmail.com
+# -----------------------------------------------------------
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
