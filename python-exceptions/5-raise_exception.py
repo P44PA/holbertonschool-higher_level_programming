fa298c4de55c0bb14a86864b9c4a5b88b59a6f51
root <dejvidpapa@gmail.com>
2024-01-08 06:32:23 -0800

    5-main.py

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print x elements of a list.
+# demonstrates how to use a try ... except for exception handling
+#
+# (C) 2024 Dejvid Papa, Tirana ,Albania
+# email dejvidpapa@gmail.com
+# -----------------------------------------------------------
+def raise_exception():
+    raise TypeError
