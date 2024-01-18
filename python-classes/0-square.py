1b59f4bb58fe85913ee6a3b637a10e46c5d829a0
root <dejvidpapa@gmail.com>
2024-01-18 12:59:35 -0800

    0-main.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program
+#
+# (C) 2024 Dejvid Papa, Tirana ,Albania
+# email dejvidpapa@gmail.com
+# -----------------------------------------------------------
+class Square:
+    """Represent a square."""
+    pass
