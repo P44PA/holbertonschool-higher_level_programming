2339089b0945a8aadc47e5cb39ed3e8eb18b088b
root <dejvidpapa@gmail.com>
2024-01-23 06:59:07 -0800

    101-main.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" LockedClass
-"""
+"""Making a module that contains a class"""
 
 
 class LockedClass:
-    __slots__= ['first_name']
+    """A class that prevents the dynamic creation of new instance attributes,
+    except for the attribute 'first_name'.
+
+    Attributes:
+    __slots__ (tuple): A tuple specifying the allowed attribute names.
+
+    Methods:
+    __setattr__(self, name, value): Overrides the default attribute setting
+        behavior to allow only the attribute 'first_name' to be set."""
+    __slots__ = ('first_name',)
