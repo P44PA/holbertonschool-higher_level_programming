1560652e70715b08c16bb0f72f7ad5ad2e59f621
root <dejvidpapa@gmail.com>
2024-01-23 07:08:58 -0800

    101-main.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""
+This is the LockedClass module
+"""
+
 
 class LockedClass:
-    """A class that prevents the dynamic creation of new instance attributes,
-    except for the attribute 'first_name'.
+    """
+    Special class that prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called first_name
+    """
 
-    Attributes:
-    __slots__ (tuple): A tuple specifying the allowed attribute names.
+    __slots__ = ['first_name']
 
-    Methods:
-    __setattr__(self, name, value): Overrides the default attribute setting
-        behavior to allow only the attribute 'first_name' to be set."""
-    __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
-
-# Example usage:
-locked_instance = LockedClass()
-locked_instance.first_name = 'John'  # Allowed
-# locked_instance.last_name = 'Doe'  # Raises AttributeError
-
+    def __init__(self, first_name=""):
+        """initializes attribute first_name"""
+        self.first_name = first_name
